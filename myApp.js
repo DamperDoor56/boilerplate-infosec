cd4af50b255d4b400bc7c1b21135ab56afdf64a4
Ludmila Lopez <ludmit@outlook.com>
2025-07-13 23:13:56 -0300

    feat: 🔒 add basic helmet configuration

--- myApp.js
+++ myApp.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const helmet = require('helmet');
 const app = express();
 
 
@@ -51,6 +52,20 @@ module.exports = app;
 const api = require('./server.js');
 app.use(express.static('public'));
 app.disable('strict-transport-security');
+// Ask Helmet to ignore the X-Powered-By header.
+app.use(helmet.hidePoweredBy());
+// Mitigate clickjacking 
+app.use(helmet.frameguard({action: 'deny'}))
+// sanitize input sent to the server
+app.use(helmet.xssFilter())
+// Hide Content-Type header
+app.use(helmet.noSniff())
+// prevents users to execute downloads in the site's context
+app.use(helmet.ieNoOpen())
+// config to use https for 90 days
+app.use(helmet.hsts({maxAge: 90*24*60*60, force: true}))
+// improve user privacy 
+app.use(helmet.dnsPrefetchControl());
 app.use('/_api', api);
 app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
