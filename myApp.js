d34eb5e6c422c4bc53b29142e4f4fe1019d83739
Ludmila Lopez <ludmit@outlook.com>
2025-07-13 23:22:52 -0300

    feat: 🔒 simplify use!

--- myApp.js
+++ myApp.js
@@ -52,27 +52,19 @@ module.exports = app;
 const api = require('./server.js');
 app.use(express.static('public'));
 app.disable('strict-transport-security');
-// Ask Helmet to ignore the X-Powered-By header.
-app.use(helmet.hidePoweredBy());
-// Mitigate clickjacking 
-app.use(helmet.frameguard({action: 'deny'}))
-// sanitize input sent to the server
-app.use(helmet.xssFilter())
-// Hide Content-Type header
-app.use(helmet.noSniff())
-// prevents users to execute downloads in the site's context
-app.use(helmet.ieNoOpen())
-// config to use https for 90 days
-app.use(helmet.hsts({maxAge: 90*24*60*60, force: true}))
-// improve user privacy 
-app.use(helmet.dnsPrefetchControl());
-// Remove cache 
-app.use(helmet.noCache());
-// Configure content policy
-app.use(helmet.contentSecurityPolicy({directives:{
-  defaultSrc: ["'self'"], // Trusted address
-  scriptSrc: ["'self'", 'trusted-cdn.com']  // Trusted addreses to download scripts from!
-}}))
+app.use(helmet({
+  frameguard: {         // configure
+    action: 'deny'
+  },
+  contentSecurityPolicy: {    // enable and configure
+    directives: {
+      defaultSrc: ["'self'"],
+      styleSrc: ['style.com'],
+    }
+  },
+  dnsPrefetchControl: false     // disable
+}))
+
 app.use('/_api', api);
 app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
